55aacb42e7cadb5ad7269b2efac1cd043a9364dd
Abhishek Bisht <abhiyanshbisht@gmail.com>
2024-08-23 12:15:46 +0530

    portfolio

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front end developer","Fresher","Technician", "Blogger", "Designer", "Freelancer","Manager"],
+        strings: ["Full Stack Developer","Front end developer", "Designer", "Python Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -48,7 +48,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front end developer","Fresher" ,"Technician", "Blogger", "Designer", "Freelancer","Manager"],
+        strings: ["Full Stack Developer","Fresher" ,"Python Developer", "Front end developer", "Back end developer", "Designer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
